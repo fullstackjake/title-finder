6bf6e6413c15d084f1cb34ca0fc5232dc8d7b448
Mr. Joseph <fullstackjake@gmail.com>
2018-09-01 01:05:01 -0400

    Confirmed Proxy response - adds axios to use

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  state = {
+    response: ''
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => {
+        this.setState({ response: res[0].TitleName })
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/films/all')
+    const body = await response.json()
+
+    if (response.status !== 200) throw Error(body.message)
+
+    return body
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Getting Titles</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
 // Use Route
 app.use('/api/films', films)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () =>
   console.log(`Server running on  http://localhost:${port}`)
